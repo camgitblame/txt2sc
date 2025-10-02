ffb709e9e99913d8b82beb86600c3d45f9ef7f4b
camgitblame <116490613+camgitblame@users.noreply.github.com>
2025-10-02 11:00:19 +0700

    Remove unused DreamBooth scripts and samplers

--- dreambooth_training/train_passengers.sh
+++ dreambooth_training/train_passengers.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
-#SBATCH --job-name=hp15
+#SBATCH --job-name=pasdb
 #SBATCH --partition=preemptgpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH --time=12:00:00
-#SBATCH --output=logs/%x_%j.out
-#SBATCH --error=logs/%x_%j.err
+#SBATCH --output=pas_log/%x_%j.out
+#SBATCH --error=pas_log/%x_%j.err
 
 
 # Hugging Face Hub 
-HF_MODEL_ID="camgitblame/hp-sd15"
-
+HF_MODEL_ID="camgitblame/passengers_sd15"
 
 # Run training
 accelerate launch train_dreambooth.py \
     --train_text_encoder \
     --pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5" \
-    --instance_data_dir="hp/data" \
-    --output_dir="hp/hp-sd15" \
-    --instance_prompt="a photo of hph harrypotter" \
+    --instance_data_dir="passengers/data" \
+    --output_dir="passengers/passengers_sd15" \
+    --instance_prompt="a photo of sks passengers" \
     --resolution=512 \
     --mixed_precision="fp16" \
     --train_batch_size=1 \
-    --learning_rate=5e-6 \
+    --learning_rate=2e-6 \
     --lr_scheduler="constant" \
     --lr_warmup_steps=0 \
     --max_train_steps=1500 \
@@ -33,5 +32,6 @@ accelerate launch train_dreambooth.py \
     --report_to wandb \
     --push_to_hub \
     --hub_model_id $HF_MODEL_ID
+
 
 
--- dreambooth_training/train_the_shining.sh
+++ dreambooth_training/train_the_shining.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
-#SBATCH --job-name=indi-sd15
+#SBATCH --job-name=shine2
 #SBATCH --partition=preemptgpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --mem=32G
 #SBATCH --time=12:00:00
-#SBATCH --output=logs/%x_%j.out
-#SBATCH --error=logs/%x_%j.err
+#SBATCH --output=shine_log/%x_%j.out
+#SBATCH --error=shine_log/%x_%j.err
 
 
 # Hugging Face Hub 
-HF_MODEL_ID="camgitblame/indiana-sd15"
+HF_MODEL_ID="camgitblame/shining_sd15_1500"
 
 
 # Run training
 accelerate launch train_dreambooth.py \
     --train_text_encoder \
     --pretrained_model_name_or_path="runwayml/stable-diffusion-v1-5" \
-    --instance_data_dir="indiana/data" \
-    --output_dir="indiana/indiana-sd15" \
-    --instance_prompt="a photo of idj indianajones" \
+    --instance_data_dir="shining/data" \
+    --output_dir="shining/shining_sd15" \
+    --instance_prompt="a photo of sks theshining" \
     --resolution=512 \
     --mixed_precision="fp16" \
     --train_batch_size=1 \
-    --learning_rate=5e-6 \
+    --learning_rate=2e-6 \
     --lr_scheduler="constant" \
     --lr_warmup_steps=0 \
-    --max_train_steps=500 \
+    --max_train_steps=1500 \
     --gradient_accumulation_steps=1 \
-    --resume_from_checkpoint="latest" \
     --report_to wandb \
     --push_to_hub \
     --hub_model_id $HF_MODEL_ID
+
 
 
